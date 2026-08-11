b092c3bb421148a8b742412f551a2393a3cf7af8
miketorg <miketorg@users.noreply.github.com>
2026-08-11 17:45:35 -0600

    Add DAY4 Group O install tile (GREEN53-55)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rhs-installs-v6";
+const CACHE = "rhs-installs-v7";
 const PRECACHE = [
   "./",
   "./index.html",
@@ -9,6 +9,7 @@ const PRECACHE = [
   "./pdfs/week1-practice.pdf",
   "./pdfs/motions.pdf",
   "./pdfs/day3teamo.pdf",
+  "./pdfs/day4groupo.pdf",
   "./manifest.json",
   "./icons/icon.svg",
 ];
